24ce1bc9f027e1cb5645bd6b011232a6549fa8b5
Ratthapon <Ratthapon@10.20.206.4>
2014-04-11 11:37:53 +0700

    refactor text segment

--- src/text/segmentation/TextSegment.java
+++ src/text/segmentation/TextSegment.java
@@ -15,41 +15,33 @@ import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
 class TextSegment extends Imgproc {
-	private static final int structureElementSize = 5;
+	private static int structureElementSize = 5;
+	private static float calibrate = structureElementSize / 2;
+	private static int charSizeThresh = 90; // px 2/3 of plat hieght
 
-	public static Mat preprocessPlate(Mat image) {
+	public static ArrayList<Mat> segmentText(Mat image) {
 		Mat plateImg = image.clone();
-		System.out.println("Segmenting Text");
-		cvtColor(plateImg, plateImg, COLOR_RGB2GRAY);
-		Highgui.imwrite("cvt.jpg", plateImg);
-		// adaptiveThreshold(plateImg, plateImg, 255,
-		// ADAPTIVE_THRESH_GAUSSIAN_C, 1, 11, 5);
-		// cvtColor(plateImg, plateImg, COLOR_BGR2GRAY);
-		// Highgui.imwrite("adaptived.jpg", plateImg);
+		ArrayList<Mat> charList = new ArrayList<Mat>();
+		resize(plateImg, plateImg, new Size(600, 270));
+		cvtColor(plateImg, plateImg, COLOR_RGBA2GRAY);
 		threshold(plateImg, plateImg, 192, 255, THRESH_BINARY);
-		Highgui.imwrite("thresh.jpg", plateImg);
 
 		// apply some dilation and erosion to join the gaps
 		Mat structureElement = getStructuringElement(MORPH_RECT, new Size(
 				structureElementSize, structureElementSize));
 		dilate(plateImg, plateImg, structureElement);
 		erode(plateImg, plateImg, structureElement);
-		Highgui.imwrite("open and close.jpg", plateImg);
 
 		// Find the contours
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 		List<MatOfPoint> boundingRectPoint = new ArrayList<MatOfPoint>();
 		List<Rect> boundingRect = new ArrayList<Rect>();
 		Mat hierarchy = new Mat();
-
 		findContours(plateImg, contours, hierarchy, RETR_LIST,
 				CHAIN_APPROX_NONE);
 		cvtColor(plateImg, plateImg, COLOR_GRAY2RGBA);
-		System.out.println("size of " + contours.size());
-		// drawContours(plateImg, contours, -1, new Scalar(255, 255,
-		// 255));
-		float calibrate = structureElementSize / 2;
-		int charSizeThresh = 90; // px 2/3 of plat hieght
+		// System.out.println("size of contours " + contours.size());
+
 		for (MatOfPoint matOfPoint : contours) {
 			Rect tempRect = boundingRect(matOfPoint);
 			MatOfPoint tmp = new MatOfPoint(new Point(tempRect.x - calibrate,
@@ -66,16 +58,26 @@ class TextSegment extends Imgproc {
 			}
 		}
 		Collections.sort(boundingRect, comparator);
-		int i = 0;
 		for (Rect rect : boundingRect) {
 			Mat cropImg = (new Mat(image, rect)).clone();
-			System.out.println("sorted x = " + rect.x);
-			Highgui.imwrite("cropchar/img_" + (i++) + ".jpg", cropImg);
+			/*
+			 //fill contour
+			cvtColor(cropImg, cropImg, COLOR_RGBA2GRAY);
+			List<MatOfPoint> charContours = new ArrayList<MatOfPoint>();
+			Mat charContoursHierarchy = new Mat();
+			findContours(cropImg, charContours, charContoursHierarchy, RETR_LIST,
+					CHAIN_APPROX_NONE);
+			drawContours(cropImg, charContours, -1, new Scalar(0, 0, 0), -1);
+			*/
+			resize(cropImg, cropImg, new Size(32, 32));
+			charList.add(cropImg);
 		}
-		drawContours(image, boundingRectPoint, -1, new Scalar(0, 255, 0), 1);
-		drawContours(image, contours, -1, new Scalar(0, 0, 255), 1);
-		Highgui.imwrite("drawBound.jpg", image);
-		return plateImg.clone();
+		/*
+		 * drawContours(image, boundingRectPoint, -1, new Scalar(0, 255, 0), 1);
+		 * drawContours(image, contours, -1, new Scalar(0, 0, 255), 1);
+		 * Highgui.imwrite("drawBound.jpg", image);
+		 */
+		return charList;
 	}
 
 	// for sort character in plate
@@ -88,10 +90,13 @@ class TextSegment extends Imgproc {
 	public static void main(String[] args) {
 		System.loadLibrary("opencv_java248");
 		Mat img;
-		Mat plateImg;
+		ArrayList<Mat> charList;
 		img = Highgui.imread("detectplate.jpg");
 		resize(img, img, new Size(600, 270));
-		plateImg = preprocessPlate(img);
-		Highgui.imwrite("preprocessed.jpg", plateImg);
+		charList = segmentText(img);
+		int i = 1;
+		for (Mat mat : charList) {
+			Highgui.imwrite("segment/char_at_" + (i++)+".jpg", mat);
+		}
 	}
 }
