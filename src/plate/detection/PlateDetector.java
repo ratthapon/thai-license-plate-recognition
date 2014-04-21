32c3e24f27d08820f60fabf4571ea196ef0f3243
Ratthapon <Ratthapon@10.20.206.2>
2014-04-21 16:58:39 +0700

    plate localize refactor text segmentation - include padding plate roi - fix char detect rule2

--- src/plate/detection/PlateDetector.java
+++ src/plate/detection/PlateDetector.java
@@ -1,6 +1,9 @@
 package plate.detection;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.opencv.core.Core;
@@ -14,49 +17,104 @@ import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
 public class PlateDetector extends Imgproc {
+	private static String logtag = "";
 	private static int strelSize = 80; // px
 	private static int threshLevel = 256; // 8bit
-	private static double detectRatio = 0.95; // 95% of rectangke
-	private static int detectSize = 10000; // px
+	private static double detectRatio = 0.5; // 95% of rectangke
+	private static int detectSizeMin = 10000; // px
+	private static int detectSizeMax = 240000; // px
+	private static double plateRatio = 20.0 / 9.0; // px
 	static int scale = 1;
 	static int delta = 0;
 
 	public static Mat detectPlate(String filename) {
 		System.loadLibrary("opencv_java248");
 		Mat image = Highgui.imread(filename);
-		resize(image, image, new Size(1024, 768));
+		resize(image, image, new Size(1366, 768));
 		return detectPlate(image);
 	}
 
-	public static Mat detectPlate(Mat image) {
+	public static Mat localizePlate(String filename) {
+		System.loadLibrary("opencv_java248");
+		Mat image = Highgui.imread(filename);
+		resize(image, image, new Size(1024, 768));
+		return localizePlate(image);
+	}
+
+	public static Mat localizePlate(Mat image) {
 		System.loadLibrary("opencv_java248");
 		Mat carImage = image.clone();
 		resize(carImage, carImage, new Size(1024, 768));
 		// preprocess plate img
 		cvtColor(carImage, carImage, COLOR_RGBA2GRAY);
-		GaussianBlur(carImage, carImage, new Size(5, 5), 3);
-		// Highgui.imwrite("log/GaussianBlur.jpg", carImage);
-		// adaptiveThreshold(carImage, carImage, 255, ADAPTIVE_THRESH_MEAN_C,
-		// THRESH_OTSU, 11, 2);
+		GaussianBlur(carImage, carImage, new Size(3, 3), 0);
 		// threshold(carImage, carImage, 0, 255, THRESH_OTSU);
-		Highgui.imwrite("log/preprocess.jpg", carImage);
-		// Sobel(carImage, carImage, ddepth, 5, 5);
-		Sobel(carImage, carImage, carImage.depth(), 1, 0, 3, scale, delta,
+		Highgui.imwrite("log/" + logtag + "/preprocess.jpg", carImage);
+		Mat soble = new Mat();
+		Sobel(carImage, soble, carImage.depth(), 1, 0, 3, scale, delta,
 				BORDER_DEFAULT);
-		Highgui.imwrite("log/edge.jpg", carImage);
+		carImage = soble.clone();
+		Highgui.imwrite("log/" + logtag + "/edge.jpg", soble);
+		// projection to x y hist
+
+		// cvtColor(graph, graph, COLOR_BGRA2RGB);
+		// MatOfPoint verticalMagnitude;
+		// MatOfPoint horizontalMagnitude;
+		List<MatOfPoint> verticalGraph = new ArrayList<MatOfPoint>();
+		List<MatOfPoint> horizontalGraph = new ArrayList<MatOfPoint>();
+		Point[] verticalMagnitude = new Point[carImage.rows()];
+		Point[] horizontalMagnitude = new Point[carImage.cols()];
+		for (int i = 0; i < carImage.rows(); i++) {
+			verticalMagnitude[i] = new Point(
+					Core.sumElems(carImage.row(i)).val[0] / 256, i);
+		}
+
+		for (int i = 0; i < carImage.cols(); i++) {
+			horizontalMagnitude[i] = new Point(i,
+					Core.sumElems(carImage.col(i)).val[0] / 256);
+			// /System.out.println("Mag at "+i+" "+Core.sumElems(carImage.col(i)).val[0]);
+		}
+		Mat graph = new Mat(image.rows(), image.cols(), image.type(),
+				new Scalar(255, 255, 255));
+		MatOfPoint mopVG = new MatOfPoint(verticalMagnitude);
+		verticalGraph.add(mopVG);
+		MatOfPoint mopHG = new MatOfPoint(horizontalMagnitude);
+		horizontalGraph.add(mopHG);
+		drawContours(graph, verticalGraph, -1, new Scalar(255, 0, 0), 1);
+		drawContours(graph, horizontalGraph, -1, new Scalar(0, 255, 0), 1);
+		return graph;
+	}
+
+	public static Mat detectPlate(Mat image) {
+		System.loadLibrary("opencv_java248");
+		Mat carImage = image.clone();
+		resize(carImage, carImage, new Size(1366, 768));
+		// preprocess plate img
+		cvtColor(carImage, carImage, COLOR_RGBA2GRAY);
+		GaussianBlur(carImage, carImage, new Size(3, 3), 5);
+		// threshold(carImage, carImage, 0, 255, THRESH_OTSU);
+		Highgui.imwrite("log/" + logtag + "/preprocess.jpg", carImage);
+		Mat sobel = new Mat();
+		Sobel(carImage, sobel, carImage.depth(), 1, 0, 3, scale, delta,
+				BORDER_DEFAULT);
+		carImage = sobel.clone();
+		Highgui.imwrite("log/" + logtag + "/edge.jpg", sobel);
 
 		// morphological img processing
-		for (int i = 1; i < strelSize; i++) {
-			Mat structureElementKernel = Imgproc.getStructuringElement(
-					Imgproc.MORPH_RECT, new Size(i, i));
-			Imgproc.dilate(carImage, carImage, structureElementKernel);
-			Imgproc.erode(carImage, carImage, structureElementKernel);
+		// for (int i = strelSize-1; i < strelSize; i++) {
+		Mat structureElementKernel = Imgproc.getStructuringElement(
+				Imgproc.MORPH_RECT, new Size(strelSize, 30));
+		Imgproc.dilate(carImage, carImage, structureElementKernel);
+		Imgproc.erode(carImage, carImage, structureElementKernel);
 
-		}
+		// }
 		// threshold(carImage, carImage, 0, 255, THRESH_OTSU);
+		Imgproc.erode(carImage, carImage, Imgproc.getStructuringElement(
+				Imgproc.MORPH_ELLIPSE, new Size(5, 5)));
 		threshold(carImage, carImage, 192, 255, THRESH_BINARY);
 		Mat thresh = carImage.clone();
-		Highgui.imwrite("log/morphological.jpg", thresh);
+
+		Highgui.imwrite("log/" + logtag + "/morphological.jpg", thresh);
 
 		// Find the contours
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
@@ -65,34 +123,35 @@ public class PlateDetector extends Imgproc {
 		Mat hierarchy = new Mat();
 
 		findContours(carImage, contours, hierarchy, RETR_LIST,
-				CHAIN_APPROX_SIMPLE);
+				CHAIN_APPROX_NONE);
 		cvtColor(carImage, carImage, COLOR_GRAY2RGBA);
 		drawContours(carImage, boundingRectPoint, -1, new Scalar(0, 255, 0), 1);
-		Highgui.imwrite("log/checkContour.jpg", carImage);
-		// System.out.println("size of contour " + contours.size());
 
-		int calibrate = strelSize / 2;
 		for (MatOfPoint matOfPoint : contours) {
 			Rect tempRect = boundingRect(matOfPoint);
-			MatOfPoint tmp = new MatOfPoint(new Point(tempRect.x - calibrate,
-					tempRect.y - calibrate), new Point(tempRect.x
-					+ tempRect.width - calibrate, tempRect.y - calibrate),
-					new Point(tempRect.x + tempRect.width - calibrate,
-							tempRect.y + tempRect.height - calibrate),
-					new Point(tempRect.x - calibrate, tempRect.y
-							+ tempRect.height - calibrate));
-			// found plate
-			if ((Core.sumElems(thresh.submat(tempRect)).val[0] / (tempRect.width
-					* tempRect.height * threshLevel)) >= detectRatio
-					&& ((tempRect.width * tempRect.height) > detectSize)) {
+			MatOfPoint tmp = new MatOfPoint();
+			Rect newRect = expandRect(tempRect, tmp, 0.00,
+					new Size(image.cols(), image.rows()));
 
+			// check condition
+			// 1. white element in rect per rect area should have more than
+			// ratio
+			boolean rule1 = (Core.sumElems(thresh.submat(newRect)).val[0] / (newRect.width
+					* newRect.height * threshLevel)) >= detectRatio;
+			// 2. rect size should around thresh
+			boolean rule2 = (newRect.width * newRect.height) > detectSizeMin
+					&& (newRect.width * newRect.height) < detectSizeMax;
+			// 3. w h ratio must nearly 20/9
+			boolean rule3 = ((double) newRect.width / (double) newRect.height) >= 1.7
+					&& ((double) newRect.width / (double) newRect.height) <= 2.6;
+			if (rule3) {
+				boundingRectPoint.add(tmp);
+				boundingRect.add(newRect);
 			}
-			boundingRectPoint.add(tmp);
-			boundingRect.add(tempRect);
 		}
-		cvtColor(thresh, thresh, COLOR_GRAY2RGB);
-		drawContours(image, boundingRectPoint, -1, new Scalar(0, 255, 0), 2);
-		Highgui.imwrite("log/drawBound.jpg", image);
+		cvtColor(sobel, sobel, COLOR_GRAY2RGB);
+		drawContours(image, boundingRectPoint, -1, new Scalar(0, 255, 0), 1);
+		Highgui.imwrite("log/" + logtag + "/drawBound.jpg", image);
 		/*
 		 * drawContours(image, boundingRectPoint, -1, new Scalar(0, 255, 0), 1);
 		 * cvtColor(thresh, thresh, COLOR_GRAY2RGB); drawContours(thresh,
@@ -101,24 +160,123 @@ public class PlateDetector extends Imgproc {
 		 * Highgui.imwrite("plate/thresh.jpg", thresh);
 		 */
 		Mat plate = new Mat();
+		Collections.sort(boundingRect, platRatioComparator);
+		for (int i = 0; i < boundingRect.size(); i++) {
+			boundingRect.set(
+					i,
+					expandRect(boundingRect.get(i), new MatOfPoint(), 0.01,
+							new Size(image.cols(), image.rows())));
+		}
+		if (boundingRect.size() <= 0) {
+			return null;
+		}
 		plate = image.submat(boundingRect.get(0)).clone();
 		resize(plate, plate, new Size(600, 270));
 		return plate;
 	}
 
+	private static Rect expandRect(Rect rect, MatOfPoint mop,
+			double expandRatio, Size imageSize) {
+		int calibrateX = 0;
+		int calibrateY = 0;
+		double padding = expandRatio;
+		Point p1, p2, p3, p4;
+		double x1, y1, x2, y2, x3, y3, x4, y4;
+		x1 = (rect.x - calibrateX) * (1.0 - padding);
+		y1 = (rect.y - calibrateY) * (1.0 - padding);
+		x2 = (rect.x + rect.width - calibrateX) * (1.0 + padding);
+		y2 = (rect.y - calibrateY) * (1.0 - padding);
+		x3 = (rect.x + rect.width - calibrateX) * (1.0 + padding);
+		y3 = (rect.y + rect.height - calibrateY) * (1.0 + padding);
+		x4 = (rect.x - calibrateX) * (1.0 - padding);
+		y4 = (rect.y + rect.height - calibrateY) * (1.0 + padding);
+		int width = (int) imageSize.width - 1;
+		int height = (int) imageSize.height - 1;
+		if (x1 < 0) {
+			x1 = 0;
+		}
+		if (x2 > width) {
+			x2 = width;
+		}
+		if (x3 > width) {
+			x3 = width;
+		}
+		if (x4 < 0) {
+			x4 = 0;
+		}
+
+		if (y1 < 0) {
+			y1 = 0;
+		}
+		if (y2 < 0) {
+			y2 = 0;
+		}
+		if (y3 > height) {
+			y3 = height;
+		}
+		if (y4 > height) {
+			y4 = height;
+		}
+
+		p1 = new Point(x1, y1);
+		p2 = new Point(x2, y2);
+		p3 = new Point(x3, y3);
+		p4 = new Point(x4, y4);
+		mop = new MatOfPoint(p1, p2, p3, p4);
+		rect = boundingRect(mop);
+		return rect;
+	}
+
+	// for sort character in plate
+	private static Comparator<Rect> platRatioComparator = new Comparator<Rect>() {
+		public int compare(Rect c1, Rect c2) {
+			int precision = 100000;
+			double ratioRect1 = Math
+					.abs(((double) c1.width / (double) c1.height) - plateRatio);
+			double ratioRect2 = Math
+					.abs(((double) c2.width / (double) c2.height) - plateRatio);
+			int diff = (int) (ratioRect1 * precision - ratioRect2 * precision);
+			return diff;
+		}
+	};
+
 	public static void main(String[] args) {
 		System.loadLibrary("opencv_java248");
-		Mat expected;
-		Mat actual;
-
-		expected = Highgui.imread("expected.jpg");
-		actual = PlateDetector.detectPlate("sourcedata/CAR1.jpg");
-		Mat dif = new Mat();
-		// Highgui.imwrite("expected.jpg",actual);
-		Highgui.imwrite("log/actual.jpg", actual);
-		// Core.absdiff(expected, actual, dif);
-		// System.out.println(dif.dump());
-		// fail("Not yet implemented");
+		Mat result;
+		String[] filename = { "CAR1.jpg", "CAR4.jpg", "CAR6.jpg", "CAR7.jpg",
+				"CAR8.jpg" };
+		for (int i = 0; i < filename.length; i++) {
+			// remove old file
+			File folder = new File("platelocalize/");
+			folder.mkdir();
+			File[] listOfFiles = folder.listFiles();
+			for (int n = 0; n < listOfFiles.length; n++) {
+				if (listOfFiles[n].isDirectory()) {
+					// System.out.println("segment/" + i + "/"
+					// + listOfFiles[n].getName());
+					File file = new File("platelocalize/"
+							+ listOfFiles[n].getName());
+					file.delete();
+				}
+			}
+			folder = new File("log/");
+			folder.mkdir();
+			listOfFiles = folder.listFiles();
+			for (int n = 0; n < listOfFiles.length; n++) {
+				if (listOfFiles[n].isDirectory()) {
+					(new File("log/" + listOfFiles[n].getName())).delete();
+				}
+			}
+			logtag = filename[i].split(".j")[0].split(".p")[0];
+			File file = new File("log/" + logtag);
+			file.mkdir();
+			result = PlateDetector.detectPlate("sourcedata/" + filename[i]);
+			if (result == null) {
+				System.out.println("image " + filename[i] + " not found");
+				continue;
+			}
+			Highgui.imwrite("platelocalize/" + filename[i], result);
+		}
 	}
 
 }
--- src/plate/text/segmentation/TextSegment.java
+++ src/plate/text/segmentation/TextSegment.java
@@ -1,20 +1,29 @@
 package plate.text.segmentation;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.io.File;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+
 public class TextSegment extends Imgproc {
+	private static String logtag = "";
 	private static int structureElementSize = 3;
 	private static double calibrate = Math.floor(structureElementSize / 2.0);
 	private static int plateHeight = 90; // px
@@ -22,16 +31,46 @@ public class TextSegment extends Imgproc {
 	private static int charSizeThresh = plateHeight * 1 / 3; // px 1/3 of plat
 																// hieght
 
+	public static ArrayList<IplImage> segmentText(IplImage iplImage) {
+		ByteBuffer iplBuffer = iplImage.getByteBuffer();
+		// Create a Matrix the same size of image
+		byte[] data;
+		data = new byte[iplBuffer.remaining()];
+		Mat image = new Mat(iplImage.height(), iplImage.width(), CvType.CV_8UC3);
+		// transfer bytes from this buffer into the given destination array
+		iplBuffer.get(data, 0, data.length);
+		// Retrieve all bytes in the buffer
+		iplBuffer.clear();
+		// Fill Matrix with image values
+		image.put(0, 0, data);
+		Highgui.imwrite("log/" + logtag + "/iplmat.jpg", image);
+
+		ArrayList<IplImage> iplImageList = new ArrayList<>();
+		ArrayList<Mat> charList;
+		charList = segmentText(image);
+		for (Mat mat : charList) {
+			data = new byte[(int) mat.size().area()];
+			BufferedImage buff = new BufferedImage(mat.width(), mat.height(),
+					BufferedImage.TYPE_4BYTE_ABGR);
+			WritableRaster raster = buff.getRaster();
+			DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+			data = dataBuffer.getData();
+			mat.get(0, 0, data);
+			IplImage img = IplImage.createFrom(buff);
+			iplImageList.add(img);
+		}
+		return iplImageList;
+	}
+
 	public static ArrayList<Mat> segmentText(Mat image) {
 		resize(image, image, new Size(plateWidth, plateHeight));
 		Mat plateImg = image.clone();
-
 		// preprocessing image
-		cvtColor(plateImg, plateImg, COLOR_RGBA2GRAY);
+		cvtColor(plateImg, plateImg, COLOR_RGB2GRAY);
 		// GaussianBlur(plateImg, plateImg, new Size(3, 3), 3);
 		threshold(plateImg, plateImg, 0, 255, THRESH_OTSU);
 		threshold(plateImg, plateImg, 0, 255, THRESH_BINARY_INV);
-		Highgui.imwrite("log/preprocess.jpg", plateImg);
+		Highgui.imwrite("log/" + logtag + "/preprocess.jpg", plateImg);
 		Mat preprocessPlate = plateImg.clone();
 
 		// apply some dilation and erosion to join the gaps
@@ -41,7 +80,7 @@ public class TextSegment extends Imgproc {
 			dilate(plateImg, plateImg, structureElement);
 			erode(plateImg, plateImg, structureElement);
 		}
-		Highgui.imwrite("log/morphological.jpg", plateImg);
+		Highgui.imwrite("log/" + logtag + "/morphological.jpg", plateImg);
 
 		// Find the contours
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
@@ -53,7 +92,6 @@ public class TextSegment extends Imgproc {
 				CHAIN_APPROX_NONE);
 		cvtColor(plateImg, plateImg, COLOR_GRAY2RGBA);
 		System.out.println("size of contours " + contours.size());
-		Highgui.imwrite("log/contours.jpg", plateImg);
 
 		// create bounding rect for crop
 		for (MatOfPoint matOfPoint : contours) {
@@ -71,14 +109,16 @@ public class TextSegment extends Imgproc {
 			boolean rule1 = tempRect.height > charSizeThresh
 					&& tempRect.height < charSizeThresh * 2;
 			// 2. char size must relate to ratio of w and h of it self
-			boolean rule2 = tempRect.width > charSizeThresh / 3
-					&& tempRect.width < charSizeThresh;
-			if (rule1 && rule2) {
+			boolean rule2 = tempRect.width > charSizeThresh /2
+					&& tempRect.width < charSizeThresh * 1.5;
+			if (rule1 && rule2 ) {
 				boundingRectPoint.add(tmp);
 				boundingRect.add(boundingRect(tmp));
 				candidateRectList.add(boundingRect(tmp));
 			}
 		}
+		drawContours(image, boundingRectPoint, -1, new Scalar(0, 255, 0), 1);
+		Highgui.imwrite("log/" + logtag + "/contours.jpg", image);
 		// 3. remove all contour that contained by other contour
 		int i = 0;
 		for (Rect inner : candidateRectList) {
@@ -94,13 +134,14 @@ public class TextSegment extends Imgproc {
 		}
 		// sort by position left to right
 		Collections.sort(boundingRect, horizontalOrderComparator);
-
+		
 		// build return value
 		ArrayList<Mat> charImageList = new ArrayList<Mat>();
 		for (Rect rect : boundingRect) {
 			Mat cropImg = (new Mat(preprocessPlate, rect)).clone();
-			threshold(cropImg, cropImg, 127, 255, THRESH_BINARY);
+			threshold(cropImg, cropImg, 127, 255, THRESH_BINARY_INV);
 			resize(cropImg, cropImg, new Size(32, 32));
+			cvtColor(cropImg, cropImg, COLOR_GRAY2RGBA);
 			charImageList.add(cropImg);
 		}
 		return charImageList;
@@ -115,32 +156,62 @@ public class TextSegment extends Imgproc {
 
 	public static void main(String[] args) {
 		System.loadLibrary("opencv_java248");
-		Mat img;
 		ArrayList<Mat> charList;
-		String[] filename = { "LP2.jpg", "LP3.jpg", "LP5.jpg", "LP6.png",
-				"LP8.jpg" };
+		// ArrayList<IplImage> iplList;
+		String[] filename = { "LP2.jpg", "LP3.jpg", "LP6.png", "LP7.jpg",
+				"LP8.jpg", "LP9.jpg", "LP10.png", "LP11.jpg", "LP5.jpg",
+				"LP12.jpg", "LP13.jpg" }; // , "LP5.jpg"
 		for (int i = 0; i < filename.length; i++) {
 			// remove old file
-			File folder = new File("segment/" + i);
+			File folder = new File("segment/");
+			folder.mkdir();
 			File[] listOfFiles = folder.listFiles();
 			for (int n = 0; n < listOfFiles.length; n++) {
-				if (listOfFiles[n].isFile()) {
-					System.out.println("segment/" + i + "/"
-							+ listOfFiles[n].getName());
-					(new File("segment/" + i + "/" + listOfFiles[n].getName()))
-							.delete();
+				if (listOfFiles[n].isDirectory()) {
+					// System.out.println("segment/" + i + "/"
+					// + listOfFiles[n].getName());
+					File file = new File("segment/" + listOfFiles[n].getName());
+					file.delete();
+				}
+			}
+			folder = new File("log/");
+			folder.mkdir();
+			listOfFiles = folder.listFiles();
+			for (int n = 0; n < listOfFiles.length; n++) {
+				if (listOfFiles[n].isDirectory()) {
+					// System.out.println("log/" + i + "/"
+					// + listOfFiles[n].getName());
+					(new File("log/" + listOfFiles[n].getName())).delete();
 				}
 			}
 
 			// create new file
 			String dirName = "sourcedata/";
-			img = Highgui.imread(dirName + filename[i]);
-			charList = segmentText(img);
-			int j = 1;
+			// IplImage img = cvLoadImage(dirName + filename[i]);
+			File file;
+			logtag = filename[i].split(".j")[0].split(".p")[0];
+			file = new File("segment/" + logtag);
+			file.mkdir();
+			file = new File("log/" + logtag);
+			file.mkdir();
+			Mat img2 = Highgui.imread(dirName + filename[i]);
+			System.out.println(logtag);
+			resize(img2, img2, new Size(75, 33));
+			charList = segmentText(img2);
+			int j;
+			j = 1;
 			for (Mat mat : charList) {
-				Highgui.imwrite("segment/" + i + "/" + filename[i] + "_"
+
+				Highgui.imwrite("segment/" + logtag + "/" + filename[i] + "_"
 						+ (j++) + ".jpg", mat);
 			}
+			/*
+			 * j = 1; for (IplImage iplImage : iplList) { file = new
+			 * File("segment/" + filename[i]); file.mkdir(); file = new
+			 * File("log/" + filename[i]); file.mkdir(); cvSaveImage("segment/"
+			 * + filename[i] + "/" + filename[i] + "_" + (j++) + ".jpg",
+			 * iplImage); }
+			 */
 		}
 
 	}
