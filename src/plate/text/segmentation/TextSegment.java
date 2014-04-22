239aa8bae4cb69ed2795380ef7c88d5ddf02a054
Ratthapon <Ratthapon@10.20.206.2>
2014-04-22 11:46:20 +0700

    refactor access modifier

--- src/plate/text/segmentation/TextSegment.java
+++ src/plate/text/segmentation/TextSegment.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
 import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,15 +24,16 @@ import org.opencv.imgproc.Imgproc;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
 public class TextSegment extends Imgproc {
-	private static String logtag = "";
-	private static int structureElementSize = 3;
-	private static double calibrate = Math.floor(structureElementSize / 2.0);
-	private static int plateHeight = 90; // px
-	private static int plateWidth = (int) (20.0 / 9.0 * plateHeight); // px
-	private static int charSizeThresh = plateHeight * 1 / 3; // px 1/3 of plat
+	private static TextSegment textSegmentObj = null;
+	private static String logtag = ""; // TODO debug var
+	private int structureElementSize = 3;
+	private double calibrate = Math.floor(structureElementSize / 2.0);
+	private int plateHeight = 90; // px
+	private int plateWidth = (int) (20.0 / 9.0 * plateHeight); // px
+	private int charSizeThresh = plateHeight * 1 / 3; // px 1/3 of plat
 																// hieght
-
-	public static ArrayList<IplImage> segmentText(IplImage iplImage) {
+	
+	private ArrayList<IplImage> segmentText(IplImage iplImage) {
 		ByteBuffer iplBuffer = iplImage.getByteBuffer();
 		// Create a Matrix the same size of image
 		byte[] data;
@@ -43,7 +45,7 @@ public class TextSegment extends Imgproc {
 		iplBuffer.clear();
 		// Fill Matrix with image values
 		image.put(0, 0, data);
-		Highgui.imwrite("log/" + logtag + "/iplmat.jpg", image);
+		Highgui.imwrite("log/" + logtag + "/iplmat.jpg", image); // TODO Log
 
 		ArrayList<IplImage> iplImageList = new ArrayList<>();
 		ArrayList<Mat> charList;
@@ -61,8 +63,15 @@ public class TextSegment extends Imgproc {
 		}
 		return iplImageList;
 	}
+	
+	public static ArrayList<Mat> getListMatOfCharImage(Mat image){
+		if (textSegmentObj == null) {
+			textSegmentObj = new TextSegment();
+		}
+		return textSegmentObj.segmentText(image);
+	}
 
-	public static ArrayList<Mat> segmentText(Mat image) {
+	private ArrayList<Mat> segmentText(Mat image) {
 		resize(image, image, new Size(plateWidth, plateHeight));
 		Mat plateImg = image.clone();
 		// preprocessing image
@@ -80,7 +89,8 @@ public class TextSegment extends Imgproc {
 			dilate(plateImg, plateImg, structureElement);
 			erode(plateImg, plateImg, structureElement);
 		}
-		Highgui.imwrite("log/" + logtag + "/morphological.jpg", plateImg);
+		Highgui.imwrite("log/" + logtag + "/morphological.jpg", plateImg);// TODO
+																			// Log
 
 		// Find the contours
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
@@ -89,9 +99,9 @@ public class TextSegment extends Imgproc {
 		List<Rect> candidateRectList = new ArrayList<Rect>();
 		Mat hierarchy = new Mat();
 		findContours(plateImg, contours, hierarchy, RETR_LIST,
-				CHAIN_APPROX_NONE);
+				CHAIN_APPROX_SIMPLE);
 		cvtColor(plateImg, plateImg, COLOR_GRAY2RGBA);
-		System.out.println("size of contours " + contours.size());
+		System.out.println("size of contours " + contours.size());// TODO Log
 
 		// create bounding rect for crop
 		for (MatOfPoint matOfPoint : contours) {
@@ -109,16 +119,16 @@ public class TextSegment extends Imgproc {
 			boolean rule1 = tempRect.height > charSizeThresh
 					&& tempRect.height < charSizeThresh * 2;
 			// 2. char size must relate to ratio of w and h of it self
-			boolean rule2 = tempRect.width > charSizeThresh /2
-					&& tempRect.width < charSizeThresh * 1.5;
-			if (rule1 && rule2 ) {
+			boolean rule2 = tempRect.width > charSizeThresh * 1 / 3
+					&& tempRect.width < charSizeThresh * 2;
+			if (rule1 && rule2) {
 				boundingRectPoint.add(tmp);
 				boundingRect.add(boundingRect(tmp));
 				candidateRectList.add(boundingRect(tmp));
 			}
 		}
 		drawContours(image, boundingRectPoint, -1, new Scalar(0, 255, 0), 1);
-		Highgui.imwrite("log/" + logtag + "/contours.jpg", image);
+		Highgui.imwrite("log/" + logtag + "/contours.jpg", image);// TODO Log
 		// 3. remove all contour that contained by other contour
 		int i = 0;
 		for (Rect inner : candidateRectList) {
@@ -134,7 +144,7 @@ public class TextSegment extends Imgproc {
 		}
 		// sort by position left to right
 		Collections.sort(boundingRect, horizontalOrderComparator);
-		
+
 		// build return value
 		ArrayList<Mat> charImageList = new ArrayList<Mat>();
 		for (Rect rect : boundingRect) {
@@ -154,65 +164,72 @@ public class TextSegment extends Imgproc {
 		}
 	};
 
-	public static void main(String[] args) {
+	private static void testSegment() {
 		System.loadLibrary("opencv_java248");
-		ArrayList<Mat> charList;
-		// ArrayList<IplImage> iplList;
-		String[] filename = { "LP2.jpg", "LP3.jpg", "LP6.png", "LP7.jpg",
-				"LP8.jpg", "LP9.jpg", "LP10.png", "LP11.jpg", "LP5.jpg",
-				"LP12.jpg", "LP13.jpg" }; // , "LP5.jpg"
-		for (int i = 0; i < filename.length; i++) {
-			// remove old file
-			File folder = new File("segment/");
-			folder.mkdir();
-			File[] listOfFiles = folder.listFiles();
-			for (int n = 0; n < listOfFiles.length; n++) {
-				if (listOfFiles[n].isDirectory()) {
-					// System.out.println("segment/" + i + "/"
-					// + listOfFiles[n].getName());
-					File file = new File("segment/" + listOfFiles[n].getName());
-					file.delete();
-				}
-			}
-			folder = new File("log/");
-			folder.mkdir();
-			listOfFiles = folder.listFiles();
-			for (int n = 0; n < listOfFiles.length; n++) {
-				if (listOfFiles[n].isDirectory()) {
-					// System.out.println("log/" + i + "/"
-					// + listOfFiles[n].getName());
-					(new File("log/" + listOfFiles[n].getName())).delete();
-				}
-			}
 
-			// create new file
-			String dirName = "sourcedata/";
-			// IplImage img = cvLoadImage(dirName + filename[i]);
-			File file;
-			logtag = filename[i].split(".j")[0].split(".p")[0];
-			file = new File("segment/" + logtag);
-			file.mkdir();
-			file = new File("log/" + logtag);
-			file.mkdir();
-			Mat img2 = Highgui.imread(dirName + filename[i]);
-			System.out.println(logtag);
-			resize(img2, img2, new Size(75, 33));
-			charList = segmentText(img2);
-			int j;
-			j = 1;
-			for (Mat mat : charList) {
-
-				Highgui.imwrite("segment/" + logtag + "/" + filename[i] + "_"
-						+ (j++) + ".jpg", mat);
+		// remove old file
+		File folder = new File("segment/");
+		folder.mkdir();
+		File[] listOfFiles = folder.listFiles();
+		for (int n = 0; n < listOfFiles.length; n++) {
+			if (listOfFiles[n].isDirectory()) {
+				// System.out.println("segment/" + i + "/"
+				// + listOfFiles[n].getName());
+				File file = new File("segment/" + listOfFiles[n].getName());
+				file.delete();
 			}
-			/*
-			 * j = 1; for (IplImage iplImage : iplList) { file = new
-			 * File("segment/" + filename[i]); file.mkdir(); file = new
-			 * File("log/" + filename[i]); file.mkdir(); cvSaveImage("segment/"
-			 * + filename[i] + "/" + filename[i] + "_" + (j++) + ".jpg",
-			 * iplImage); }
-			 */
+		}
+		folder = new File("log/");
+		folder.mkdir();
+		listOfFiles = folder.listFiles();
+		for (int n = 0; n < listOfFiles.length; n++) {
+			if (listOfFiles[n].isDirectory()) {
+				// System.out.println("log/" + i + "/"
+				// + listOfFiles[n].getName());
+				(new File("log/" + listOfFiles[n].getName())).delete();
+			}
 		}
 
+		ArrayList<Mat> charList;
+		String[] filename = { "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg",
+				"6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.png",
+				"12.jpg", "13.jpg", "14.png", "15.jpg", "16.jpg", "17.jpg",
+				"18.jpg" }; // , "LP5.jpg"
+		String[] ans = { "กก35", "กจ99", "กด7171", "กค9535", "ก2219", "ปพ6945",
+				"ฎผ6557", "ฎก7700", "ชห9515", "จร638", "กอ9999", "กท9999",
+				"กท1000", "983597", "704928", "๔๒๗๔๙", "06469", "40ม2", };
+		for (int i = 0; i < filename.length; i++) {
+			// create new file
+			String dirName = "sourcedata/LP/";
+			logtag = filename[i].split(".j")[0].split(".p")[0];
+			System.out.println("LOGTAG " + logtag + " read "
+					+ (dirName + filename[i]));
+			(new File("segment/" + logtag)).mkdir();
+			(new File("log/" + logtag)).mkdir();
+			if ((new File(dirName + filename[i])).exists()) {
+				Mat plateImage = Highgui.imread(dirName + filename[i]);
+				charList = getListMatOfCharImage(plateImage);
+				int j;
+				j = 1;
+				try {
+					(new File("segment/" + logtag + "/" + ans[i]))
+							.createNewFile();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("LP length " + charList.size());
+				for (Mat mat : charList) {
+					Highgui.imwrite("segment/" + logtag + "/" + logtag + "_"
+							+ (j++) + ".jpg", mat);
+				}
+			} else {
+				System.out.println("File not found");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		testSegment();
 	}
 }
